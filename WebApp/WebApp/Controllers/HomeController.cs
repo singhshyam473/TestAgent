e857d3d360565de74655410fb361b99e5e3e2564
Shyam Mohite <shyam.mohite@adrosonic.com>
2020-03-15 18:53:09 +0530

    test contact

--- WebApp/WebApp/Controllers/HomeController.cs
+++ WebApp/WebApp/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace WebApp.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message ="Contact details";
 
             return View();
         }
